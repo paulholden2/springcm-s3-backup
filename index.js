c4d6397a50819068f215c48bd96bf93e35d86bb6
Paul Holden <pholden@stria.com>
2017-12-22 09:46:00 -0800

    Update with building folderlist

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
+const dotenv = require('dotenv');
 const AWS = require('aws-sdk');
 const SpringCM = require('springcm-node-sdk');
 const async = require('async');
 
+dotenv.config();
+
+function subfolders(folder, callback) {
+	SpringCM.folder.folders(folder, (err, folders) => {
+		if (err) {
+			return callback(err);
+		}
+
+		console.log(`${folder.name} subfolders: ${folders.length}`);
+
+		if (folders.length > 0) {
+			async.eachSeries(folders, (folder, callback) => {
+				subfolders(folder, (err, folderlist) => {
+					if (err) {
+						return callback(err);
+					}
+
+					folders = folders.concat(folderlist);
+					callback();
+				});
+			}, (err) => {
+				if (err) {
+					return callback(err);
+				}
+
+				callback(null, folders);
+			});
+		} else {
+			callback(null, []);
+		}
+	});
+}
+
 async.waterfall([
+	// SpringCM auth
 	(callback) => {
+		SpringCM.auth.login(process.env.SPRINGCM_DATACENTER, process.env.SPRINGCM_CLIENT_ID, process.env.SPRINGCM_CLIENT_SECRET, (err, token) => {
+			if (err) {
+				return callback(err);
+			}
+
+			callback();
+		});
+	},
+	(callback) => {
+		SpringCM.folder.root((err, root) => {
+			if (err) {
+				return callback(err);
+			}
+
+			callback(null, root);
+		});
+	},
+	(root, callback) => {
+		subfolders(root, (err, folderlist) => {
+			if (err) {
+				return callback(err);
+			}
+
+			callback(null, folderlist);
+		});
+	},
+	(folderlist, callback) => {
+		console.log(folderlist.length);
 		callback();
 	}
 ], (err) => {
