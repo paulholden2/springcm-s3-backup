b875aeec8cf0033a84afaa226fb5f855a80d3096
Paul Holden <pholden@stria.com>
2018-09-06 10:06:21 -0700

    More whitespace fix

--- index.js
+++ index.js
@@ -6,48 +6,48 @@ const clusg = require('command-line-usage');
 const backup = require('./backup.js');
 
 const commands = [
-	null,
-	'backup',
-	'restore'
+  null,
+  'backup',
+  'restore'
 ];
 
 const options = [
-	{
-		name: 'verbose',
-		alias: 'v',
-		description: 'Enables verbose logging of events and operations.',
-		type: Boolean
-	},
-	{
-		name: 'config',
-		alias: 'c',
-		description: 'The path to the .env file to use.',
-		type: String
-	},
-	{
-		name: 'no-log',
-		description: 'Don\'t log backup to Zoho Reports',
-		type: Boolean
-	},
-	{
-		name: 'overwrite',
-		alias: 'o',
-		description: 'Always update content in S3, even if it is already recent',
-		type: Boolean
-	},
-	{
-		name: 'parallel',
-		alias: 'p',
-		description: 'How many concurrent requests to run. Must be >= 1',
-		defaultValue: 10,
-		type: Number
-	},
-	{
-		name: 'help',
-		alias: 'h',
-		description: 'Displays this usage dialog.',
-		type: Boolean
-	}
+  {
+    name: 'verbose',
+    alias: 'v',
+    description: 'Enables verbose logging of events and operations.',
+    type: Boolean
+  },
+  {
+    name: 'config',
+    alias: 'c',
+    description: 'The path to the .env file to use.',
+    type: String
+  },
+  {
+    name: 'no-log',
+    description: 'Don\'t log backup to Zoho Reports',
+    type: Boolean
+  },
+  {
+    name: 'overwrite',
+    alias: 'o',
+    description: 'Always update content in S3, even if it is already recent',
+    type: Boolean
+  },
+  {
+    name: 'parallel',
+    alias: 'p',
+    description: 'How many concurrent requests to run. Must be >= 1',
+    defaultValue: 10,
+    type: Number
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    description: 'Displays this usage dialog.',
+    type: Boolean
+  }
 ];
 
 const cmd = clcmd(commands, process.argv.slice(2, process.argv.length));
@@ -55,34 +55,34 @@ const command = cmd.command;
 const argv = cmd.argv;
 
 const opts = clarg(options, {
-	argv: argv
+  argv: argv
 });
 
 const sections = [
-	{
-		header: 'SpringCM AWS S3 Backup',
-		content: 'Performs iterative backups of a SpringCM account to AWS S3'
-	},
-	{
-		header: 'Options',
-		optionList: options
-	}
+  {
+    header: 'SpringCM AWS S3 Backup',
+    content: 'Performs iterative backups of a SpringCM account to AWS S3'
+  },
+  {
+    header: 'Options',
+    optionList: options
+  }
 ];
 
 if (!command || opts.help || opts.parallel < 1) {
-	return console.log(clusg(sections));
+  return console.log(clusg(sections));
 }
 
 if (opts.config) {
-	dotenv.config({
-		path: path.join(__dirname, opts.config)
-	});
+  dotenv.config({
+    path: path.join(__dirname, opts.config)
+  });
 }
 
 if (command === 'backup') {
-	backup(opts);
+  backup(opts);
 } else if (command === 'restore') {
-	console.log('Restore not supported yet');
+  console.log('Restore not supported yet');
 } else {
-	console.log(clusg(sections));
+  console.log(clusg(sections));
 }
